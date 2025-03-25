f2e2548d01f477622c46ec49c8c77772e4f335c3
NailethR <nait.0005@gmail.com>
2025-03-24 22:26:40 -0300

    remove all

--- components/sections/projects/laptop-animation.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
-import animation from "@/public/lottie/laptop.json";
-
-export default function LaptopAnimation({ className }: { className?: string }) {
-  return <Lottie className={className} animationData={animation} loop={true} />;
-}
